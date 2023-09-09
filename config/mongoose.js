9f77ddbb2a58f60792146c51dec6ac730aa6e984
tomkjose <tomjosekunnumpurath@gmail.com>
2023-09-09 15:55:03 +0530

    latest commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/MovieDB");
+require("dotenv").config();
+const movieDB = process.env.DB;
+mongoose.connect(movieDB);
 const db = mongoose.connection;
 
 db.on("error", () => {
