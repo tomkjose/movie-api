ccc78d8df088742de414bfdae99d14bf8d3d140b
tomkjose <tomjosekunnumpurath@gmail.com>
2023-09-09 15:35:31 +0530

    new commit

--- controllers/user_api.js
+++ controllers/user_api.js
@@ -46,3 +46,12 @@ module.exports.signUp = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 };
+
+module.exports.update = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body);
+    res.status(201).json({ message: "User Details Updated" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
