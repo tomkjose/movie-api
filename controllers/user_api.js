beb3e7c7b09c906b2015fb58c370307813d7e5f4
tomkjose <tomjosekunnumpurath@gmail.com>
2023-09-13 03:31:26 +0530

    user details

--- controllers/user_api.js
+++ controllers/user_api.js
@@ -1,6 +1,8 @@
-const User = require("../models/User");
+const Users = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
 require("dotenv").config();
 const secret_key = process.env.SECRET_KEY;
 
@@ -9,7 +11,7 @@ module.exports.signIn = async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    const user = await User.findOne({ email: email });
+    const user = await Users.findOne({ email: email });
 
     if (!user) {
       return res.status(401).json({
@@ -35,7 +37,7 @@ module.exports.signUp = async (req, res) => {
   console.log("SignUp");
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const user = new User({
+    const user = new Users({
       username: req.body.username,
       email: req.body.email,
       password: hashedPassword,
@@ -49,8 +51,21 @@ module.exports.signUp = async (req, res) => {
 
 module.exports.update = async (req, res) => {
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body);
+    const user = await Users.findByIdAndUpdate(req.params.id, req.body);
     res.status(201).json({ message: "User Details Updated" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports.fetchUserDetails = async (req, res) => {
+  try {
+    const userId = mongoose.Types.ObjectId(req.params.id);
+    const user = await Users.findOne(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -6,4 +6,5 @@ const UserController = require("../../../controllers/user_api");
 router.post("/signin", UserController.signIn);
 router.post("/signup", UserController.signUp);
 router.put("/update/:id", UserController.update);
+router.get("/:id", UserController.fetchUserDetails);
 module.exports = router;
